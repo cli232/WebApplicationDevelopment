d430bb95b50d0892df00fbd7cae75f1af694c5b8
cli232 <cli232@my.centennialcollege.ca>
2023-09-14 16:18:44 -0400

    commit

---
+++ Semester6/WebApplicationDevelopment/Lab2-1/server.js
@@ -0,0 +1,39 @@
+const connect = require('connect');
+
+const app = connect();
+
+function logger(req, res, next) {
+
+    console.log(req.method, req.url);
+
+    next();
+
+ };
+
+function helloWorld(req, res, next) {
+
+   res.setHeader('Content-Type', 'text/plain');
+
+   res.end('Hello World');
+
+};
+
+function goodbyeWorld(req, res, next) {
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('Good bye World');
+
+ };
+
+ 
+
+app.use(logger);
+
+app.use('/hello', helloWorld);
+
+app.use('/goodbye',goodbyeWorld);
+
+app.listen(3000);
+
+console.log('Server running at http://localhost:3000/');
